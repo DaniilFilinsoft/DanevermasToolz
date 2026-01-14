a0656e41f2187546906858a66c3897c822f252e9
DaniilFilinsoft <daniilgre4ishkin@yandex.ru>
2026-01-14 15:53:01 +0300

    Update Данины тузлы.py

--- Данины тузлы.py
+++ Данины тузлы.py
@@ -10,6 +10,11 @@ import time
 def cl():
     for aaa in range(64):
         print()
+def cl1():
+    try:
+        os.system(cls)
+    except:
+        cl()
 def menugen(ilist, text):
     while keyboard.is_pressed("enter"):
         time.sleep(0)
@@ -27,10 +32,7 @@ def menugen(ilist, text):
             select = select + 1
             if select == len(ilist):
                 select = 0
-            try:
-                os.system('cls')
-            except:
-                cl()
+            cl1()
             print(text)
             print()
             print(colored("используй w/s для перемещения курсора, enter для выборки",'dark_grey'))
@@ -45,10 +47,7 @@ def menugen(ilist, text):
             select = select - 1
             if select < 0:
                 select = len(ilist)-1
-            try:
-                os.system('cls')
-            except:
-                cl()
+            cl1()
             print(text)
             print()
             print(colored("используй w/s для перемещения курсора, enter для выборки",'dark_grey'))
@@ -63,36 +62,46 @@ def menugen(ilist, text):
         time.sleep(0)
     return(select+1)
 while aac != 5:
-    aac = menugen(["Скретч","М.Я.У. (бета)","OSINT","О создателях","Выйти"], "DanevermasToolZ 1.0")
-    try:
-        os.system(cls)
-    except:
-        cl()
+    aac = menugen(["Скретч","М.Я.У. (бета)","глаз боба","О создателях","Выйти"], "DanevermasToolZ 1.0.1")
+    cl1()
     if aac == 5:
         pass
     if aac == 3:
-        print('Во первых, осинт это не слишом хорошо, во вторых, мне лень его сейчас пилить, пользуйся другими фичами приложухи [enter]')
+        cl1()
+        print('паааатоооом сдееелаааююююю [enter]')
         while not keyboard.is_pressed("enter"):
             if keyboard.is_pressed("enter"):
                 pass
     if aac == 2:
-        print('МЯУ (Массированный ядерный удар) будет готов... ...когда-то. Следи за обновами, чтож')
+        cl1()
+        print('МЯУ (Массированный ядерный удар) будет готов... ...когда-то. Следи за обновами, чтож [enter]')
         while not keyboard.is_pressed("enter"):
             if keyboard.is_pressed("enter"):
                 pass
     if aac == 4:
+        cl1()
         print('Приложуху создал:', colored('@MrDaneverma','cyan'),'(тг)')
         print('Спасибо', colored('TimMcCool','cyan'),'за ScratchAttach, без него раздел Скретч не работал-бы')
+        print('Ещё', colored('@gervatyy','cyan'),'(тг) хотел бранчнуть и говорил жди пул реквест а в итоге где ано')
         print('Нажми ENTER что-бы выйти')
         while not keyboard.is_pressed("enter"):
             if keyboard.is_pressed("enter"):
                 pass
     if aac == 1:
+        cl1()
         aad = menugen(["Залогинится","Глянуть инфу о акках данивермы","Быстрое","Назад"], "Шо делать будем?")
+        if aad = 3:
+            cl1()
+            print("Уважаемые люди, сделайте тут просмоторщик профилей, у нас тут исходный код открытый") #MrDaneverma
+            while not keyboard.is_pressed("enter"):
+                if keyboard.is_pressed("enter"):
+                    pass
         if aad == 4:
             pass
         if aad == 2:
-            print('вообще, эта фича есть только В НАСТОЯЩЕЙ консоли данивемы которая есть ТОЛЬКО У МЕНЯ, так как чтоб узнать, забанен\n-ли акк, нужно знать пароль')
+            cl1()
+            print('У меня есть идея как это реализовать, но не сейчас, жди 1.1')
+            print('(это будет костыльный костыль)')
             print('нажми ентер чтоб выйти')
             while not keyboard.is_pressed("enter"):
                 if keyboard.is_pressed("enter"):
@@ -100,10 +109,7 @@ while aac != 5:
         if aad == 1:
             while aae == 0:
                 aae = 1
-                try:
-                    os.system(cls)
-                except:
-                    cl()
+                cl1()
                 print("Напиши ник: (ОНО СНАЧАЛО СИЛЬНО ТУПИТ НИЧЕГО НЕКОТОРОЕ ВРЕМЯ НЕ НАЖИМАЙ Я НЕ ЗНАЮ ПОЧЕМУ ОНО ТАК)")
                 ni = input('>')
                 print("А теперь пароль: (Я их никуда не сохраняю, можешь в коде проверить)")
@@ -112,7 +118,7 @@ while aac != 5:
                 try:
                     session = sa.login(str(ni), str(pa))
                 except:
-                    print("Данные входа неверны")
+                    print("войти не получилосъ")
                     time.sleep(3)
                     aae = 0
             if aae == 1:
@@ -120,6 +126,7 @@ while aac != 5:
                     while aaf != 4:
                         aaf = menugen(["Узнать свои данные (не показывай это остальным!)","Хакнуть облачные переменные (тут есть ещё логи)","Активности на сайте","Выйти из акка и отсюда"], "Ты залогинился как "+ni+", шо хочешь на скретче сделать?")
                         if aaf == 1:
+                            cl1()
                             user = session.connect_user(ni)
                             print(colored("Инфа о тебе:",'green'))
                             print("Сэшн айди:",colored(session.id,"cyan"))
@@ -132,7 +139,7 @@ while aac != 5:
                                 print("Неверифнутый емэил:",colored(session.new_email_address,"cyan"))
                             except:
                                 print('У тебя',colored("нет","green"),"неверифнутого емэила")
-                            print("А ты почку подтвердил кракадил?:",colored(session.has_outstanding_email_confirmation,"cyan"))
+                            print("Ты эл.почту подтвердил?:",colored(session.has_outstanding_email_confirmation,"cyan"))
                             print("Ты присоеденился:",colored(user.join_date,"cyan"))
                             print("Ты о себе:",colored(user.about_me,"cyan"))
                             print("Ты работаешь над:",colored(user.wiwo,"cyan"))
@@ -145,6 +152,7 @@ while aac != 5:
                                 if keyboard.is_pressed("enter"):
                                     pass
                 else:
+                    cl1()
                     print("слющай, акк",colored("забанен.",'red'))
                     print("Нажми ентер чтоб вернутся")
                     while not keyboard.is_pressed("enter"):
@@ -153,4 +161,5 @@ while aac != 5:
                             pa = ""
                             pass
                     
+
 
